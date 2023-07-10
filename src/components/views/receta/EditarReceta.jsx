d1e8d3d6b59c2635236cffd7aaccf2c194d8256a
Juan Gerardo Romero Uro <jgromerouro@gmail.com>
2023-07-10 19:52:29 -0300

    se hizo la lógica de editar receta

--- src/components/views/receta/EditarReceta.jsx
+++ src/components/views/receta/EditarReceta.jsx
@@ -1,9 +1,10 @@
-import { Container, Row, Col, Form, Button, Alert } from "react-bootstrap";
-import Sidebar from "../../common/Sidebar";
-import { useForm } from "react-hook-form";
-import { consultaReceta } from "../../helpers/queries";
-import { useEffect } from "react";
-import { useNavigate, useParams } from "react-router-dom";
+import { Container, Row, Col, Form, Button, Alert } from 'react-bootstrap';
+import Sidebar from '../../common/Sidebar';
+import { useForm } from 'react-hook-form';
+import { consultaEditarReceta, consultaReceta } from '../../helpers/queries';
+import { useEffect } from 'react';
+import { useNavigate, useParams, Link } from 'react-router-dom';
+import Swal from 'sweetalert2';
 
 const EditarReceta = () => {
   const {
@@ -11,6 +12,7 @@ const EditarReceta = () => {
     handleSubmit,
     formState: { errors },
     reset,
+    setValue,
   } = useForm();
   const { id } = useParams();
   const navegacion = useNavigate();
@@ -18,26 +20,42 @@ const EditarReceta = () => {
   useEffect(() => {
     consultaReceta(id).then((respuesta) => {
       if (respuesta) {
-        console.log("tengo que cargar el objeto en el formulario");
+        console.log('tengo que cargar el objeto en el formulario');
         console.log(respuesta);
-        setValue("nombreReceta", respuesta.nombreReceta);
-        setValue("descripcionReceta", respuesta.descripcionReceta);
-        setValue("ingredientes", respuesta.ingredientes);
-        setValue("instrucciones", respuesta.instrucciones);
-        setValue("imagen", respuesta.imagen);
-        setValue("categoria", respuesta.categoria);
+        setValue('nombreReceta', respuesta.nombreReceta);
+        setValue('descripcionReceta', respuesta.descripcionReceta);
+        setValue('ingredientes', respuesta.ingredientes);
+        setValue('instrucciones', respuesta.instrucciones);
+        setValue('imagen', respuesta.imagen);
+        setValue('categoria', respuesta.categoria);
       } else {
         Swal.fire(
-          "Ocurrio un error",
+          'Ocurrio un error',
           `No se puede editar la receta, intentelo mas tarde`,
-          "error"
+          'error'
         );
       }
     });
   }, []);
 
-  const onSubmit = (recetaNueva) => {
-    console.log(recetaNueva);
+  const onSubmit = (receta) => {
+    console.log(receta);
+    consultaEditarReceta(receta, id).then((respuestaEditado) => {
+      if (respuestaEditado && respuestaEditado.status === 200) {
+        Swal.fire(
+          'REceta Editada',
+          `La receta ${receta.nombreReceta} fue editada correctamente`,
+          'success'
+        );
+        navegacion('/administrador');
+      } else {
+        Swal.fire(
+          'Ocurrio un error',
+          `El producto ${receta.nombreReceta} no fue editada, intentelo mas tarde`,
+          'error'
+        );
+      }
+    });
   };
 
   return (
@@ -47,25 +65,35 @@ const EditarReceta = () => {
         <Col md={9}>
           <aside className="flex-grow-aside text-light">
             <hr />
-            <h1 className="display-4 mt-3">Nueva Receta</h1>
+            <h1 className="display-4 mt-3">Editar Receta</h1>
             <hr />
+            <Row>
+              <Col className="d-flex justify-content-end">
+                <Link
+                  to="/administrador"
+                  className="btn btn-secondary d-sm-block mt-2 mb-3 btnBlock"
+                >
+                  Volver Atrás
+                </Link>
+              </Col>
+            </Row>
             <Form onSubmit={handleSubmit(onSubmit)}>
               <Form.Group className="mb-3" controlId="formNombreReceta">
                 <Form.Label>Nombre Receta*</Form.Label>
                 <Form.Control
                   type="text"
                   placeholder="Ej: Vasos de Crema de Vainilla con frutos rojos"
-                  {...register("nombreReceta", {
-                    required: "El nombre de la receta es obligatorio",
+                  {...register('nombreReceta', {
+                    required: 'El nombre de la receta es obligatorio',
                     minLength: {
                       value: 2,
                       message:
-                        "La cantidad minima de caracteres es de 2 digitos",
+                        'La cantidad minima de caracteres es de 2 digitos',
                     },
                     maxLength: {
                       value: 120,
                       message:
-                        "La cantidad máxima de caracteres es de 120 digitos",
+                        'La cantidad máxima de caracteres es de 120 digitos',
                     },
                   })}
                 />
@@ -81,17 +109,17 @@ const EditarReceta = () => {
                   as="textarea"
                   placeholder="Ej: Una sola cucharadita de estos vasitos basta para rendirse totalmente a ella y convertirla en LA CREMA DE VAINILLA, con mayúsculas, por excelencia."
                   className="altoDeArea"
-                  {...register("descripcionReceta", {
-                    required: "La descripción de la receta es obligatoria",
+                  {...register('descripcionReceta', {
+                    required: 'La descripción de la receta es obligatoria',
                     minLength: {
                       value: 2,
                       message:
-                        "La cantidad minima de caracteres es de 2 digitos",
+                        'La cantidad minima de caracteres es de 2 digitos',
                     },
                     maxLength: {
                       value: 500,
                       message:
-                        "La cantidad máxima de caracteres es de 500 digitos",
+                        'La cantidad máxima de caracteres es de 500 digitos',
                     },
                   })}
                 />
@@ -107,17 +135,17 @@ const EditarReceta = () => {
                   as="textarea"
                   placeholder="Ej: 330 g nata líquida, 2 vainas de vainilla, 55 g de azúcar, etcétera..."
                   className="altoDeArea"
-                  {...register("ingredientes", {
-                    required: "Los ingredientes de la receta son obligatorios.",
+                  {...register('ingredientes', {
+                    required: 'Los ingredientes de la receta son obligatorios.',
                     minLength: {
                       value: 2,
                       message:
-                        "La cantidad minima de caracteres es de 2 digitos",
+                        'La cantidad minima de caracteres es de 2 digitos',
                     },
                     maxLength: {
                       value: 600,
                       message:
-                        "La cantidad máxima de caracteres es de 600 digitos",
+                        'La cantidad máxima de caracteres es de 600 digitos',
                     },
                   })}
                 />
@@ -133,17 +161,17 @@ const EditarReceta = () => {
                   as="textarea"
                   placeholder="Ej: 1. El día anterior corta longitudinalmente las vainas de vainilla y raspa las semillas. 2. Pon vainas y semillas con la nata en un recipiente hermético y deja que macere en la nevera. 3. ---"
                   className="altoDeArea"
-                  {...register("instrucciones", {
-                    required: "Los ingredientes de la receta son obligatorios.",
+                  {...register('instrucciones', {
+                    required: 'Los ingredientes de la receta son obligatorios.',
                     minLength: {
                       value: 2,
                       message:
-                        "La cantidad minima de caracteres es de 2 digitos",
+                        'La cantidad minima de caracteres es de 2 digitos',
                     },
                     maxLength: {
                       value: 1300,
                       message:
-                        "La cantidad máxima de caracteres es de 1300 digitos",
+                        'La cantidad máxima de caracteres es de 1300 digitos',
                     },
                   })}
                 />
@@ -158,13 +186,13 @@ const EditarReceta = () => {
                 <Form.Control
                   type="text"
                   placeholder="Ej: https://images.pexels.com/photos/16132921/pexels-photo-16132921/free-photo-of-comida-mercado-fruta-grande.jpeg"
-                  {...register("imagen", {
-                    required: "La imagen es un dato obligatorio.",
+                  {...register('imagen', {
+                    required: 'La imagen es un dato obligatorio.',
                     pattern: {
                       value:
                         /^(http(s?):)([/|.|\w|\s|-])*\.(?:png|jpe?g|gif|svg)$/,
                       message:
-                        "La URL de la imagen debe cumplir con por ej: http://imagen.com/img.jpg",
+                        'La URL de la imagen debe cumplir con por ej: http://imagen.com/img.jpg',
                     },
                   })}
                 />
@@ -178,8 +206,8 @@ const EditarReceta = () => {
                 <Form.Label>Categoria*</Form.Label>
                 <Form.Select
                   aria-label="Seleccione una categoría:"
-                  {...register("categoria", {
-                    required: "Debe seleccionar una categoria...",
+                  {...register('categoria', {
+                    required: 'Debe seleccionar una categoria...',
                   })}
                 >
                   <option value="">Seleccione una opcion</option>
